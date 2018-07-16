f58c70c3984c543709f0d4c7f1c933d11982af80
unknown <davidchincharashvili@gmail.com>
2018-07-16 11:41:36 +0400

    a

--- js/main.js
+++ js/main.js
@@ -1023,6 +1023,28 @@ var vars = {
 };
 
 
+function fetchJSONFile(path, callback) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                var data = JSON.parse(httpRequest.responseText);
+                if (callback) callback(data);
+            }
+        }
+    };
+    httpRequest.open('GET', path);
+    httpRequest.send(); 
+}
+
+
+fetchJSONFile('99.json', function(data){
+    
+    console.log(data);
+});
+
+
+
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);

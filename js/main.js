18ccf8ae7e017afc898a88ec8e3b959c0b42ff66
david <davidchincharashvili@gmail.com>
2018-07-29 02:48:24 +0400

    remove slim js

--- js/main.js
+++ js/main.js
@@ -24,20 +24,26 @@ var vars = {
     library: {}
 };
 
-$.get('js/999.json', function(d){vars.library = d[0];});
+$.get('js/999.json', function (d) {
+    vars.library = d[0];
+});
 
 
 function addZeros(numb) {
-    numb = numb<10? "00" + numb : "0" + numb;
+    numb = numb < 10 ? "00" + numb : "0" + numb;
     return numb;
 }
+
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
+
 function getImg() {
-    var numb, iMin = vars.minMax.minimum, iMax = vars.minMax.maximum;
+    var numb, 
+        iMin = vars.minMax.minimum,
+        iMax = vars.minMax.maximum;
     var inputMinIsEmpty = iMin.val() === "";
     var inputMaxIsEmpty = iMax.val() === "";
     var localMinIsEmpty = localStorage.minimum == undefined;
@@ -51,15 +57,13 @@ function getImg() {
         if (localMinIsEmpty && localMinIsEmpty) {
             numb = getRandomInt(vars.minMax.min, vars.minMax.max);
             // console.warn('from default parameters');
-        }
-        else {
+        } else {
             numb = getRandomInt(localStorage.minimum, localStorage.maximum);
             // console.warn('from localStorage');
             iMin.val(localStorage.minimum);
             iMax.val(localStorage.maximum);
         }
-    }
-    else {
+    } else {
         numb = getRandomInt(iMin.val(), iMax.val());
         localStorage.minimum = iMin.val();
         localStorage.maximum = iMax.val();
@@ -69,14 +73,17 @@ function getImg() {
     numb = addZeros(numb);
     return numb;
 }
+
 function imgDigitsToName(digits) {
     return vars.library[digits];
 }
+
 function imgDigitsToPath(digits) {
     var src = vars.main.img[0].src;
 
     return src.slice(0, src.length - 7) + digits + ".jpg";
 }
+
 function generateArray(from, to) {
     var element = [];
     for (var index = from; index <= to; index++) {
@@ -85,6 +92,7 @@ function generateArray(from, to) {
     }
     return element;
 }
+
 function generateImageLinks(from, to) {
     var links = [];
     var arr = generateArray(from, to);
@@ -94,6 +102,7 @@ function generateImageLinks(from, to) {
     }
     return links;
 }
+
 function appendImages(elem, min, max) {
     var images = generateImageLinks(min, max);
     for (var _i = 0, images_1 = images; _i < images_1.length; _i++) {
@@ -102,41 +111,47 @@ function appendImages(elem, min, max) {
     }
     // console.log(min, max, images);
 }
+
 function delElements(className) {
     $(className).remove();
 }
+
 function incScore(a) {
     return vars.scores[a]++;
 }
+
 function showScores(a, b) {
     $(vars.buttons[a]).html(b);
 }
+
 function showDigits(digits) {
     $(vars.main.cover).html(digits);
 }
+
 function change() {
     var imageDigits = getImg();
     // console.log(
     // imageDigits,
-        imgDigitsToName(imageDigits),
-        imgDigitsToPath(imageDigits),
-        showDigits(imageDigits);
-        
+    imgDigitsToName(imageDigits),
+    imgDigitsToPath(imageDigits),
+    showDigits(imageDigits);
+
     $(vars.main.img).attr('src', imgDigitsToPath(imageDigits));
     setTimeout(function () {
         $(vars.main.img).attr('alt', imgDigitsToName(imageDigits));
     }, 1000);
     return true;
 }
+
 function redMoves(winlose) {
     var red = vars.scores.red;
     if (winlose == "win") {
         $(red).width($(red).width() - 2);
-    }
-    else {
+    } else {
         $(red).width($(red).width() + 2);
     }
 }
+
 function btnCliecked(winlose, yesno) {
     change();
     var wlScore = incScore(winlose);
@@ -145,12 +160,19 @@ function btnCliecked(winlose, yesno) {
     vars.main.cover.show();
 }
 // button Events
-vars.buttons.no.click(function () { btnCliecked("lose", "no"); });
-vars.buttons.yes.click(function () { btnCliecked("win", "yes"); });
-vars.buttons.learn.click(function () { appendImages('.learn', vars.minMax.minimum.val(), vars.minMax.maximum.val()); });
-vars.buttons.remLearn.click(function () { delElements('.img-to-remove'); });
+vars.buttons.no.click(function () {
+    btnCliecked("lose", "no");
+});
+vars.buttons.yes.click(function () {
+    btnCliecked("win", "yes");
+});
+vars.buttons.learn.click(function () {
+    appendImages('.learn', vars.minMax.minimum.val(), vars.minMax.maximum.val());
+});
+vars.buttons.remLearn.click(function () {
+    delElements('.img-to-remove');
+});
 vars.main.cover.click(function () {
     $(this).hide();
 });
 change();
-//# sourceMappingURL=main.js.map
